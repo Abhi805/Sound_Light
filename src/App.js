641ce3300fc9837d389d1722d2fc8ff0bad92657
Abhishek Rathore <abhishekrathore1921@gmail.com>
2025-04-03 19:36:02 +0530

    gfdfgf

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Services from "./Pages/Services/Services";
+import Events from "./Pages/Event/Event";
+import Equipment from "./Pages/Equipment/Equipment";
+import Contact from "./Pages/Contact/Contact";
+
+const App = () => {
   return (
-<<<<<<< HEAD
-  <>
-        
-  </>
-  )
-=======
-    <div>
+    <Router>
       <Navbar />
-      <h1>Welcome to Sound & Light</h1>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/equipment" element={<Equipment />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
->>>>>>> 35152ab9ca27527d46676fecaaa854b5e6d62caa
-}
+};
 
 export default App;
-
